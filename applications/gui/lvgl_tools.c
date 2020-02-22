abdaab586d80b6fbca7579538b54cc05afe53748
Hehesheng <hhslzzyyes@outlook.com>
2020-02-22 22:48:36 +0800

    导入成功

--- applications/gui/lvgl_tools.c
+++ applications/gui/lvgl_tools.c
@@ -1,5 +1,44 @@
 #include "lvgl_tools.h"
 
+struct __lvgl_obj_base
+{
+    char *name;
+    char *text;
+};
+
+extern const int __lvgl_apps_tab_start;
+extern const int __lvgl_apps_tab_end;
+static const void *lvgl_apps_start_point = &__lvgl_apps_tab_start;
+static const void *lvgl_apps_end_point   = &__lvgl_apps_tab_end;
+
+LVGL_APP_ITEM_EXPORT(fun, LV_SYMBOL_AUDIO, NULL)
+LVGL_APP_ITEM_EXPORT(info, LV_SYMBOL_DRIVE, NULL)
+LVGL_APP_ITEM_EXPORT(terminal, LV_SYMBOL_BACKSPACE, NULL)
+LVGL_APP_ITEM_EXPORT(half, LV_SYMBOL_UP, NULL)
+
+LVGL_WIDGET_ITEM_EXPORT(fun, NULL, NULL)
+
+static void tab_test(void)
+{
+    struct __lvgl_obj_base *obj;
+    struct __lvgl_obj_base **point;
+
+    rt_kprintf("start: 0x%X\n", lvgl_apps_start_point);
+    rt_kprintf("end: 0x%X\n", lvgl_apps_end_point);
+
+    for (point = (struct __lvgl_obj_base **)lvgl_apps_start_point; point < (struct __lvgl_obj_base **)lvgl_apps_end_point;
+         point++)
+    {
+        obj = *point;
+
+        rt_kprintf("point:0x%X\n", point);
+        rt_kprintf("obj:0x%X\n", obj);
+        rt_kprintf("name: %s\n", obj->name);
+        rt_kprintf("text: %s\n", obj->text);
+    }
+}
+MSH_CMD_EXPORT(tab_test, test);
+
 lv_obj_t *lv_find_obj_parent_by_type(lv_obj_t *obj, char *name)
 {
     lv_obj_t *ret = obj;
--- applications/gui/lvgl_tools.h
+++ applications/gui/lvgl_tools.h
@@ -1,8 +1,43 @@
-#ifndef __LVGL_TOOLS_H__ 
-#define __LVGL_TOOLS_H__ 
+#ifndef __LVGL_TOOLS_H__
+#define __LVGL_TOOLS_H__
 
 #include <lvgl.h>
 
+struct __lvgl_app_item
+{
+    const char *name;
+    const char *text;
+    const void *img;
+    lv_event_cb_t cb;
+};
+
+#define LVGL_APP_ITEM_EXPORT(text, img, cb)                                                       \
+    const char __lvgl_app_item_##text##_##img##_name[] SECTION(".rodata.lvgl") = "__app_" #text;  \
+    const char __lvgl_app_item_##text##_##img##_text[] SECTION(".rodata.lvgl") = #text;           \
+                                                                                                  \
+    const struct __lvgl_app_item __lvgl_app_item_##text##_##img##_obj SECTION(".rodata.lvgl") = { \
+        __lvgl_app_item_##text##_##img##_name, __lvgl_app_item_##text##_##img##_text, img, cb};   \
+    RT_USED const void *__lvgl_app_##text##_##img##_point SECTION("LVObjTab") = &__lvgl_app_item_##text##_##img##_obj;
+
+struct __lvgl_widget_item
+{
+    const char *name;
+    const char *text;
+    lv_obj_t **obj;
+    lv_event_cb_t cb;
+};
+
+#define LVGL_WIDGET_ITEM_EXPORT(text, widget, cb)                                                          \
+    const char __lvgl_widget_item_##text##_##widget##_name[] SECTION(".rodata.name") = "__widget_" #text;  \
+    const char __lvgl_widget_item_##text##_##widget##_text[] SECTION(".rodata.name") = #text;              \
+    lv_obj_t *__lvgl_widget_item_##text##_##widget##_widget                          = widget;             \
+                                                                                                           \
+    const struct __lvgl_widget_item __lvgl_widget_item_##text##_##widget##_obj SECTION(".rodata.name") = { \
+        __lvgl_widget_item_##text##_##widget##_name, __lvgl_widget_item_##text##_##widget##_text,          \
+        &__lvgl_widget_item_##text##_##widget##_widget, cb};                                               \
+    RT_USED const void *__lvgl_widget_##text##_##widget##_point SECTION("LVObjTab") =                      \
+        &__lvgl_widget_item_##text##_##widget##_obj;
+
 lv_obj_t *lv_find_obj_parent_by_type(lv_obj_t *obj, char *name);
 
-#endif	// __LVGL_TOOLS_H__
+#endif  // __LVGL_TOOLS_H__
